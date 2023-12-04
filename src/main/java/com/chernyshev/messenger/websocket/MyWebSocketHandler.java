1e55404c12cdbd15d0a2dcfb5474d95448efc160
Pavel Chernyshev <pasha7w@gmail.com>
2023-12-04 15:18:07 +0300

    Добавил вебсокет

--- src/main/java/com/chernyshev/messenger/websocket/MyWebSocketHandler.java
+++ src/main/java/com/chernyshev/messenger/websocket/MyWebSocketHandler.java
@@ -81,55 +81,5 @@ public class MyWebSocketHandler implements WebSocketHandler {
     public boolean supportsPartialMessages() {
         return false;
     }
-//    @Override
-//    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-//        String[] segments = Objects.requireNonNull(session.getUri()).getPath().split("/");
-//        Long receiverId = Long.valueOf(segments[segments.length - 1]);
-//        UserEntity receiver = userRepository.findById(receiverId).orElseThrow();
-//        session.getAttributes().put("receiver", receiver.getUsername());
-//        String sender = Objects.requireNonNull(session.getPrincipal()).getName();
-//
-//        if (!userSessions.containsKey(sender)) {
-//            userSessions.put(sender, new HashMap<>());
-//        }
-//        userSessions.get(sender).put(receiver.getUsername(), session);
-//
-//        List<MessageResponse> messageHistory = messageService.getMessageHistory(sender, receiverId);
-//        for (MessageResponse messageResponse : messageHistory) {
-//            session.sendMessage(new TextMessage(messageResponse.getSentAt()
-//                    + " " + messageResponse.getReceiver() + " : " + messageResponse.getText()));
-//        }
-//    }
-//    @Override
-//    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-//        String sender = session.getPrincipal().getName();
-//        String receiver = (String) session.getAttributes().get("receiver");
-//
-//        try {
-//            WebSocketSession receiverSession = userSessions.get(receiver).get(sender);
-//            MessageResponse newMessage = messageService.sendMessage(sender, userRepository.findByUsername(receiver).orElse(null).getId(), message.getPayload());
-//            if (receiverSession != null) {
-//                receiverSession.sendMessage(new TextMessage(newMessage.getSentAt() + " " + newMessage.getSender() + " : " + newMessage.getText()));
-//            }
-//        } catch (FriendshipException e) {
-//            session.sendMessage(new TextMessage("Не удалось отправить сообщение, так как пользователь использует приватные настройки аккаунта"));
-//        } catch (UsernameNotFoundException e){
-//            session.sendMessage(new TextMessage("Не удалось отправить сообщение, пользователь "+ receiver+" не существует"));
-//        } catch (Exception e){
-//            session.sendMessage(new TextMessage("Возникла ошибка при отправке сообщения"));
-//        }
-//    }
-//    @Override
-//    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-//        String sender = session.getPrincipal().getName();
-//        String receiver = (String) session.getAttributes().get("receiver");
-//
-//        if (userSessions.containsKey(sender)) {
-//            userSessions.get(sender).remove(receiver);
-//            if (userSessions.get(sender).isEmpty()) {
-//                userSessions.remove(sender);
-//            }
-//        }
-//    }
 }
 
